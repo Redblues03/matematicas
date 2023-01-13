f024650846380c4a6bc5d65a42921c472c087e83
Redblue03 <andres.serrano6316@gmail.com>
2023-01-12 19:45:20 -0500

    tercera modificación

--- area.py
+++ area.py
@@ -1,4 +1,10 @@
 def areaCuadrado():
     lado=float(input("ingrese el valor dle lado: "))      #12 de enero de 2023
     return print("el área del cuadrado es: ", lado **2) 
-areaCuadrado()
+areaCuadrado() #13 de enero 23
+
+def areaRectangulo():
+    base=float(input("ingrese el valor de la base: "))
+    altura=float(input("ingrese el valor de la altura: "))        
+    return print("el área del cuadrado es: ", base * altura) 
+areaRectangulo()
